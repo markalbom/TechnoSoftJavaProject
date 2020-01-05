9b89f219880551d83442d7740eb229bfed8e057a
mark <mjalbom@gmail.com>
2020-01-05 13:24:20 -0500

    additional question answered

--- SRC/MyFirstPackage/Loops.java
+++ SRC/MyFirstPackage/Loops.java
@@ -41,15 +41,15 @@ public class Loops {
         //FOR-LOOPS
             // Exercise 1. Count up to 100
 
-//        for (int i = 0; i < 100; i++){      //initializer (where does the loop begin), condition (run code until), incrementer (increment during each iteration)
-//            System.out.println(i);
-//        }
+        for (int i = 0; i < 100; i++){      //initializer (where does the loop begin), condition (run code until), incrementer (increment during each iteration)
+            System.out.println(i);
+        }
 //
 //        //Exercise 2.  Write a for-loop that prints from 100, descends in steps of 5
 //
-//        for (int i = 100; i > 0; i = i - 5){
-//            System.out.println(i);
-//        }
+        for (int i = 100; i > 0; i = i - 5){
+            System.out.println(i);
+        }
 
         //Exercise 3.  Write a for-loop that will reverse a string
 
@@ -63,10 +63,8 @@ public class Loops {
         System.out.println(reversedName);
 
 
-//        char [] arrName = name.split("");
-//        for (int = arrName[0]; i < arrName.length; i++){
-//            System.out.println(i);
-//        }
+//        char [] arrName = name.split(""); -- another way to split string into a char array
+//
 
 
         //Exercise 3.  Write a for loop that checks to see if a string is a palindrome
@@ -77,12 +75,51 @@ public class Loops {
             reversedWord = reversedWord + stringArray[i];
         }
         System.out.println(reversedWord);
-        if (word.equals(reversedWord)){
-            System.out.println("this word is a palindrome");
+        if (word.equals(reversedWord)){     //better to use 'equals' rather than 'matches' re: java doc keywords
+            System.out.println(word + " and " + reversedWord + " " + "are palindromes");
         } else {
-            System.out.println("this is not a palindrome");
+            System.out.println(word + " and " + reversedWord + " " + "are not palindromes");
         }
+// alternative method:
 
+        // return word.equals(reversedWord)
+
+
+        //HW
+          //1.  based on the percentage you find, assign a letter value to the student
+
+
+    double score = 24;  //needs to be double to be able to calculate the average (int will only yield integer 0 every time)
+    double maxScore = 25;
+    double percentage = (score / maxScore) * 100;
+
+    if (percentage <= 59){
+        System.out.println("your average is " + percentage + " " + "and so you have received an \'F\'");
+    } else if (percentage >= 60 && percentage <= 69){
+        System.out.println("your average is " + percentage + " " + "and so you have received an \'D\'");
+    } else if (percentage >= 70 && percentage <= 79){
+        System.out.println("your average is " + percentage + " " + "and so you have received an \'C\'");
+    } else if (percentage >= 80 && percentage <= 89){
+        System.out.println("your average is " + percentage + " " + "and so you have received an \'B\'");
+    } else if (percentage >= 90 && percentage <= 100){
+        System.out.println("your average is " + percentage + " " + "and so you have received an \'A\' - congratulations!");
+    } else {
+        System.out.println("you have entered an incorrect percentage");
+    }
+
+//3.  Write logic to check to see if a number is a prime number
+        int number = 7;
+        for (int i = 2; i < number; i++){  //i has to begin at 2 since 1 is a prime number;
+            if (number % i == 0){
+                System.out.println(number + " is not a prime number");
+            } else {
+                System.out.println(number + " is a prime number");
+            }
+        }
 
     }
 }
+
+
+
+
